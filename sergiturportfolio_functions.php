bc7ba5b0c3cb049d6abb606a130b0b2827973352
Sergi Tur Badenas <sergitur@ebretic.com>
2015-02-07 13:12:36 +0100

    Sync

---
+++ sergiturportfolio_functions.php
@@ -0,0 +1,10 @@
+add_shortcode( 'acacha_twitter_follow', 'acacha_twitter_follow' );
+function acacha_twitter_follow( $atts, $content = null ) {
+
+}
+
+add_shortcode( 'hola_mon', 'hola_mon' );
+function hola_mon() {
+	 return 'Hola mon!';
+}
+
