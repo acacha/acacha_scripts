813e369d9965ae4c2f8a78406fc14b5c243faf34
root <root@acacha.org>
2015-02-07 12:43:20 +0100

    First version

---
+++ sergiturportfolio.js
@@ -0,0 +1,11 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2005-TODAY Sergi Tur Badenas http://acacha.org/sergitur
+// http://acacha.org
+
+function hello_world() {
+  alert("hello!");
+}
+
+
+
